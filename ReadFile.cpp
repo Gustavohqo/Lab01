f1e34ccf430f8f93ec61b8dda76a5f5163f0f0ad
Gustavo Henrique Queiroz de Oliveira <gustavo.hqo@gmail.com>
2014-07-09 14:16:15 -0500

    ReadyFile is compiling, I think haha

--- ReadFile.cpp
+++ ReadFile.cpp
@@ -1,43 +1,42 @@
-//#include "ReadFile.h"
+#include "ReadFile.h"
 #include <iostream>
 #include <string>
 
 //class "constructor"
 ReadFile::ReadFile(const char* file_name)
 {
-
    input_file.open(file_name);
    closed = false;
-   eof = false;
+   _eof = false;
 }
 
-void ReadFile::~ReadFile()
+ReadFile::~ReadFile()
 {
-   close(rf);
-   delete rf;
+   close();
+   //delete ReadFile[];
 }
 
 bool ReadFile::eof()
 {
-   return rf->_eof;
+   return _eof;
 }
 
 void ReadFile::close()
 {
-   if (!rf->closed)
+   if (!closed)
    {
-      rf->input_file.close();
-      rf->closed = true;
+      input_file.close();
+      closed = true;
    }
 }
 
 String* ReadFile::readLine()
 {
-   if (rf->closed) return NULL;
-   if (rf->_eof) return NULL;
+   if (closed) return NULL;
+   if (_eof) return NULL;
 
    string text;
-   rf->_eof = !(getline(rf->input_file, text));
+   _eof = !(getline(input_file, text));
 
    String* str = new String(text.c_str());
    return str;
